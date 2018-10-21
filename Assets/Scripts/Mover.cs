29a6ca1b96dbcfc5e8446f7ab747d4c39cc3780a
oopsyscoops <oopsyscoops@gmail.com>
2018-10-21 16:44:06 -0400

    added laser bolt prefab wooooo

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	private Rigidbody rigidbody;
+
+	public float speed;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+
+		rigidbody.velocity = transform.forward * speed;
+	}
+}
