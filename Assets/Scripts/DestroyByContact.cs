9ce60a7bd8702f8b52ae8ac99818d99fec75013f
jkumm <jkumm@bigvikinggames.com>
2018-10-23 07:47:10 -0400

    Now we have a prefab for the asteroid that includes explosions on contact with a laser and/or the player ship

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion;
+    public GameObject playerExplosion;
 
 	private void OnTriggerEnter(Collider other) {
 		if (other.tag == "Boundary") {
 			return;
 		}
 
-		Destroy(other.gameObject);
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
+        Destroy(other.gameObject);
 		Destroy(gameObject);
 	}
 }
